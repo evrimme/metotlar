576b3bfdc2dd066f38aa2ac9ad9292c4fc29c540
Evrim <evrimeliacik12@gmail.com>
2024-12-02 22:06:43 +0300

    Add files via upload
    Girilen a sayısının b ninci dereceden kuvvetini geri döndüren metot.

---
+++ deger_donduren_metot3/deger_donduren_metot3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deger_donduren_metot3
+{
+    internal class Program
+    {
+        static int UsAl(int a,int b)
+        {
+            int cevap=(int)Math.Pow(a,b);
+            return cevap;
+        }
+        static void Main(string[] args)
+        {
+            // Girilen a sayısının b ninci dereceden kuvvetini geri döndüren metotu yazın.
+            Console.Write("a sayısını giriniz: ");
+            int a=int.Parse(Console.ReadLine());
+
+            Console.Write("b sayısını giriniz: ");
+            int b=int.Parse(Console.ReadLine());
+
+            int sonuc = UsAl(a, b);
+            Console.WriteLine("Sonuc = " + sonuc);
+
+            Console.ReadKey();
+        }
+    }
+}
