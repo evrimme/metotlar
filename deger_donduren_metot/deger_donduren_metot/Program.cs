bf5559841302f5ce11e5178c4834189b01a3b473
Evrim <evrimeliacik12@gmail.com>
2024-12-02 22:03:46 +0300

    Add files via upload
    Konsoldan girilen 3 sayının toplamını geri döndüren metot.

---
+++ deger_donduren_metot/deger_donduren_metot/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deger_donduren_metot
+{
+    internal class Program
+    {
+        static int Topla3Sayi(int a,int b,int c)
+        {
+            int cevap = a + b + c;
+            return cevap;
+        }
+        static void Main(string[] args)
+        {
+            int sayi1; int sayi2; int sayi3;
+            //Konsoldan girilen 3 sayının toplamını geri döndüren metotu yazınız.       
+            Console.Write("1. sayıyı giriniz: ");
+            sayi1=int.Parse(Console.ReadLine());
+
+            Console.Write("2. sayıyı giriniz: ");
+            sayi2 = int.Parse(Console.ReadLine());
+
+            Console.Write("3. sayıyı giriniz: ");
+            sayi3 = int.Parse(Console.ReadLine());
+
+            int sonuc = Topla3Sayi(sayi1,sayi2,sayi3);
+            Console.WriteLine("Sonuç = "+sonuc);
+            Console.ReadKey();
+        }
+    }
+}
