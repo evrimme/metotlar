8f5beb22b9927c724fafe90d19c14c20d8a471f7
Evrim <evrimeliacik12@gmail.com>
2024-12-02 22:05:30 +0300

    Add files via upload
    Girilen sayının küpünü geri döndüren metot.

---
+++ deger_donduren_metot2/deger_donduren_metot2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deger_donduren_metot2
+{
+    internal class Program
+    {
+        static int KupAl(int n)
+        {
+            int cevap = n * n * n;
+            return cevap;
+        }
+        static void Main(string[] args)
+        {
+            // Girilen sayının küpünü geri döndüren metotu yazınız.
+
+            Console.Write("Sayı giriniz: ");
+            int sayi=int.Parse(Console.ReadLine());
+
+            int sonuc = KupAl(sayi);
+            Console.WriteLine("Sonuç = " + sonuc);
+
+            Console.ReadKey();
+        }
+    }
+}
