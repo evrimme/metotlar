817a51e9b48b9f3201688b796fc8b6cd8d2381d0
Evrim <evrimeliacik12@gmail.com>
2024-12-02 22:07:34 +0300

    Add files via upload
    Girilen sayının faktöriyelini geri döndüren metot.

---
+++ deger_donduren_metot4/deger_donduren_metot4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deger_donduren_metot4
+{
+    internal class Program
+    {
+        static int Faktoriyel(int n)
+        {
+            int fak = 1;
+            for(int i=1; i<=n; i++)
+            {
+                fak = fak * i;
+            }
+            return fak;
+        }
+        static void Main(string[] args)
+        {
+            // Girilen sayının faktöriyelini geri döndüren metotu yazınız.
+            Console.Write("Sayı giriniz: ");
+            int sayi=int.Parse(Console.ReadLine());
+
+            int sonuc = Faktoriyel(sayi);
+            Console.WriteLine("Sayının faktöriyeli = " + sonuc);
+
+            Console.ReadKey();
+        }
+    }
+}
